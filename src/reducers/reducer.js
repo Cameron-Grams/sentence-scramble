a06acdb5d79a57f5f19fb5401068d0afa9bdeaea
Cameron Grams <cameron.grams.2@gmail.com>
2018-12-10 11:30:20 -0500

    with reducer

---
+++ src/reducers/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    originalText : "",
+    sentences : []
+}
+
+function Reducer( state = initialState, action){
+    switch( action.type ){
+        default:
+            return { ...state }
+    }
+}
